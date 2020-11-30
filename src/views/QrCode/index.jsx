83a78083e4e0d25eecde24a34a8c8e25ba89ae83
Fabio Menozzi <f.menozzi@yahoo.com>
2020-11-29 21:40:08 -0300

    WIP: qrcode

--- src/views/QrCode/index.jsx
+++ src/views/QrCode/index.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import * as S from './styles';
 
+import Qr from 'qrcode.react';
+
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
@@ -12,10 +14,16 @@ function QrCode() {
             <Footer />
             <S.Content>
                 <h1>Use the QRCODE to Login</h1>
-                <S.QrCodeArea>
-                    
-                </S.QrCodeArea>
                 <p>Your activities will be sync with your smartphone</p>
+                <S.QrCodeArea>
+                    <Qr value='getmacaddress' size={350} />
+                </S.QrCodeArea> 
+
+                <S.ValidationCode>
+                    <span>Type the number that you received in your smartphone!</span>
+                    <input type='text' />
+                    <button type='button'> SYNC</button>
+                </S.ValidationCode>
             </S.Content>
         </S.Container>
     )
